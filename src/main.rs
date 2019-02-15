ad29514690749d72f362040961954a8e3f3e0d0c
Aron Strandberg <arostr-5@student.ltu.se>
2019-02-15 23:51:38 +0100

    resolved EoF, need to add more parsing

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ extern crate quick_xml;
 
 
 use std::io;
-// use std::io::BufReader;
 use std::fs::File;
-// use quick_xml::Reader;
 use quick_xml::events::Event;
 
 
@@ -31,33 +29,32 @@ fn fetch_xml() {
 
 fn read_file() {
 
-    // let file = File::open("station_data").unwrap();
-    // let buffered = BufReader::new(file);
-    // let mut reader = Reader::from_reader(buffered);
-    let mut xml = quick_xml::Reader::from_file("station_data").expect("Failed to open file!");
+    let mut xml = quick_xml::Reader::from_file("station_data.xml").expect("Failed to open file!");
     xml.trim_text(true);
 
 
-    let mut txt = Vec::new();
+    // let mut txt = Vec::new();
     let mut buf = Vec::new();
 
     loop {
         match xml.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) if e.name() == b"ns0:roadNumber" => {
-                txt.push(
-                    xml
-                        .read_text(b"ns0:roadNumber", &mut Vec::new())
-                        .expect("Could not decode text value"),
-                );
-                println!("{:?}", txt);
-            }
-            Ok(Event::Eof) => break, // END
-            Err(e) => panic!("Error at position {}: {:?}", xml.buffer_position(), e),
-            _ => (),       
 
+            Ok(Event::Start(ref e)) => {
+
+                match e.name() {
+                    b"ns0:measurementSiteRecord" => println!("id: {}", &xml.read_text(e.attributes(), &mut Vec::new().unwrap()), 
+                    b"ns0:roadNumber" => println!("{}", &xml.read_text(e.name(), &mut Vec::new()).unwrap()),
+
+                    _ => (),    
+                }
+
+            },
+            Ok(Event::Eof) => break,
+            Err(e) => panic!("Error {}: {:?}", xml.buffer_position(), e),
+            _ => (),
+            
         }
         buf.clear();
-
     }
 
 
