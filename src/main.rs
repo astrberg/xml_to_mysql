af71d1213e02e4845a304682ac46e1d331a38722
Aron Strandberg <arostr-5@student.ltu.se>
2019-03-08 12:56:23 +0100

    handling threads

--- src/main.rs
+++ src/main.rs
@@ -13,45 +13,36 @@ mod database;
 
 fn main() {
 
-    println!("RCM XML to MySQL started...");
-
-
-    let user = "LTU";
-    let pass = "DatexLTU2018#";
-    let station_url = "https://datex.trafikverket.se/D2ClientPull/MetaDataBA/2_3/WeatherMetaData";
-    let weather_url = "https://datex.trafikverket.se/D2ClientPull/WeatherPullServerBA/2_3/Weather";   
-    let opts = database::get_opts("mysql", "password", "127.0.0.1", "db");
-
+    // println!("RCM XML to MySQL started...");
 
     // Station data fetched once every day from DATEX II, parsed and inserted to MYSQL
-    let station_thread = thread::spawn(move || loop {
+    thread::spawn(|| loop {
         // Fetch file
-        fetch::fetch_xml(&station_url, user, pass, "station_data_cache.xml");
-       
+        fetch::fetch_xml("https://datex.trafikverket.se/D2ClientPull/MetaDataBA/2_3/WeatherMetaData", "DatexLTU2018#", "LTU", "station_data_cache.xml");
+        let opts = database::get_opts("mysql", "password", "127.0.0.1", "db");   
         // Parse XML file and insert to MYSQL
         let station_data = parse_xml::parse_station("station_data_cache.xml");
-        database::insert_station_data(opts.clone(), station_data);
+        database::insert_station_data(opts, station_data);
         
         // sleep 24 h
         thread::sleep(stdtime::Duration::from_secs(86400));
 
     });
     // Weather data fetched every 15 min from DATEX II, parsed and inserted to MYSQL
-    let weather_thread = thread::spawn(move || loop {
+    thread::spawn(|| loop {
         // Fetch file
-        fetch::fetch_xml(weather_url, user, pass, "weather_data_cache.xml");
-
+        fetch::fetch_xml("https://datex.trafikverket.se/D2ClientPull/WeatherPullServerBA/2_3/Weather", "DatexLTU2018#", "LTU", "weather_data_cache.xml");
+        let opts = database::get_opts("mysql", "password", "127.0.0.1", "db");
         // Parse XML file and insert to MYSQL
         let weather_data = parse_xml::parse_weather("weather_data_cache.xml");
-        database::insert_weather_data(opts.clone(), weather_data);
+        database::insert_weather_data(opts, weather_data);
 
         // sleep 15 min
         thread::sleep(stdtime::Duration::from_secs(900));
 
     });
+    // let opts = database::get_opts("mysql", "password", "127.0.0.1", "db");
 
-
-    station_thread.join().unwrap();
-    weather_thread.join().unwrap();
+    // database::create_mysql_tables(opts);
 
 }
